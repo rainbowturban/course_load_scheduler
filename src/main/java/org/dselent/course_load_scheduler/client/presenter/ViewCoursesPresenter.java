cfb270800213ca3a4a3bce9341c0814bcbdb8540
hjauris <hmjauris@wpi.edu>
2018-02-11 16:37:20 -0500

    Added stuff for View Course page
    Should have all the necessary files, but nothing actually useful in them yet.

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/ViewCoursesPresenter.java
@@ -0,0 +1,5 @@
+package org.dselent.course_load_scheduler.client.presenter;
+
+public interface ViewCoursesPresenter extends BasePresenter {
+	//add functions that we need besides the basePresenter interface stuff!
+}
---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/ViewCoursesPresenterImpl.java
@@ -0,0 +1,23 @@
+package org.dselent.course_load_scheduler.client.presenter.impl;
+
+import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
+import org.dselent.course_load_scheduler.client.presenter.ViewCoursesPresenter;
+import org.dselent.course_load_scheduler.client.view.BaseView;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public class ViewCoursesPresenterImpl extends BasePresenterImpl implements ViewCoursesPresenter{
+
+	@Override
+	public void go(HasWidgets container) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BaseView<? extends BasePresenter> getView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
