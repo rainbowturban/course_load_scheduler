0ac4517798c38cb3e7dce635875b3e670f1c75cf
Sarah <saogrady@wpi.edu>
2018-02-17 17:50:54 -0500

    again

---
+++ src/main/resources/org/dselent/course_load_scheduler/client/view/impl/HomeViewImpl.java
@@ -0,0 +1,19 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class HomeViewImpl extends Composite {
+
+	private static HomeViewImplUiBinder uiBinder = GWT.create(HomeViewImplUiBinder.class);
+
+	interface HomeViewImplUiBinder extends UiBinder<Widget, HomeViewImpl> {
+	}
+
+	public HomeViewImpl() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
