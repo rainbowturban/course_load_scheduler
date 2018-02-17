dfb4a46b83b5e25a0fc1189a02e6d795f51a94df
Frank Campanelli <rainbowturban412@gmail.com>
2018-02-17 18:28:55 -0500

    got rid of dumb merge thingys

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/AdminCalendarPresenter.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/AdminCalendarPresenter.java
@@ -8,11 +8,6 @@ public interface AdminCalendarPresenter extends BasePresenter
 {
 	IndexPresenter getParentPresenter();
 	void setParentPresenter(IndexPresenter parentPresenter);
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> 4834805425ca539cc10c5c37d359e4ed512c833c
 	void fillRequests();
 	List<RequestTables> retrieveRequests();
 	//loads Home page (TODO: work out parameters, differentiate btwn admin and user homepages?)
