9b0dde979c14cfa3d82840a2ac214fcd99ff5f8c
Matt <mjschueler@gmail.com>
2018-02-17 18:26:02 -0500

    added borders to request panel

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/AdminCalendarPresenterImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/AdminCalendarPresenterImpl.java
@@ -80,34 +80,34 @@ public class AdminCalendarPresenterImpl extends BasePresenterImpl implements Adm
 		RequestTables request1 = new RequestTables();
 		request1.setCoursesNumber("CS3733");
 		request1.setCoursesTitle("Software Engineering");
-		request1.setEndTime(null);
+		request1.setStartTime(null);
 		request1.setRequestOtherMessage("Please give me this class in this term");
 		request1.setRequestStatus("Approved");
 		request1.setRequestsId(1);
 		request1.setRequestsUserId(1);
-		request1.setStartTime(null);
+		request1.setEndTime(null);
 		request1.setTermsName("A");
 
 		RequestTables request2 = new RequestTables();
 		request2.setCoursesNumber(null);
 		request2.setCoursesTitle(null);
-		request2.setEndTime(new Time(32400));
+		request2.setStartTime(new Time(32400000));
 		request2.setRequestOtherMessage("Please give me a class between these times in this term");
 		request2.setRequestStatus("Rejected");
 		request2.setRequestsId(2);
 		request2.setRequestsUserId(3);
-		request2.setStartTime(new Time(35400));
+		request2.setEndTime(new Time(35400000));
 		request2.setTermsName("B");
 
 		RequestTables request3 = new RequestTables();
 		request3.setCoursesNumber("ES3011");
 		request3.setCoursesTitle("Controls Engineering");
-		request3.setEndTime(new Time(36000));
+		request3.setStartTime(new Time(36000000));
 		request3.setRequestOtherMessage("Give me this class at this time in this term");
 		request3.setRequestStatus("Unresolved");
 		request3.setRequestsId(3);
 		request3.setRequestsUserId(2);
-		request3.setStartTime(new Time(39000));
+		request3.setEndTime(new Time(39000000));
 		request3.setTermsName("D");
 
 		requests.add(request1);
@@ -127,31 +127,34 @@ public class AdminCalendarPresenterImpl extends BasePresenterImpl implements Adm
 
 
 		while(iterator.hasNext()) {
+			VerticalPanel newPanel = new VerticalPanel();
 			RequestTables requestInfo = iterator.next();
 			Label userIdLabel = new Label("user id: " + requestInfo.getRequestsUserId());
-			panel.add(userIdLabel);
+			newPanel.add(userIdLabel);
 			Label statusLabel = new Label("status: " + requestInfo.getRequestStatus());
-			panel.add(statusLabel);
+			newPanel.add(statusLabel);
 			if (requestInfo.getCoursesNumber() != null) {
 				Label courseNumberLabel = new Label("course number: " + requestInfo.getCoursesNumber());
-				panel.add(courseNumberLabel);
+				newPanel.add(courseNumberLabel);
 				Label courseTitleLabel = new Label("course name: " + requestInfo.getCoursesTitle());
-				panel.add(courseTitleLabel);
+				newPanel.add(courseTitleLabel);
 			}
 			Label termsLabel = new Label("term: " + requestInfo.getTermsName());
-			panel.add(termsLabel);
+			newPanel.add(termsLabel);
 			if (requestInfo.getStartTime() != null) {
 				Label startTimeLabel = new Label("start time: " + requestInfo.getStartTime());
-				panel.add(startTimeLabel);
+				newPanel.add(startTimeLabel);
 			}
 			if (requestInfo.getEndTime() != null) {
 				Label endTimeLabel = new Label("end time: " + requestInfo.getEndTime());
-				panel.add(endTimeLabel);
+				newPanel.add(endTimeLabel);
 			}
 			if (requestInfo.getRequestOtherMessage() != null) {
 				Label messageLabel = new Label("message: " + requestInfo.getRequestOtherMessage());
-				panel.add(messageLabel);
+				newPanel.add(messageLabel);
 			}
+			newPanel.setBorderWidth(5);
+			panel.add(newPanel);
 		}
 
 		//view.setCourseList(panel);
