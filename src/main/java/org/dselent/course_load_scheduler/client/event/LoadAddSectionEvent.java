ecd219c34e1a238f9c959b9c27f3c1f5af588b29
hjauris <hmjauris@wpi.edu>
2018-02-17 13:29:27 -0500

    Fixed Function name

--- src/main/java/org/dselent/course_load_scheduler/client/event/LoadAddSectionEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/LoadAddSectionEvent.java
@@ -1,5 +1,41 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class LoadAddSectionEvent {
+import org.dselent.course_load_scheduler.client.action.LoadAddSectionAction;
+import org.dselent.course_load_scheduler.client.event_handler.LoadAddSectionEventHandler;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class LoadAddSectionEvent extends GwtEvent<LoadAddSectionEventHandler>{
+	public static Type<LoadAddSectionEventHandler> TYPE = new Type<LoadAddSectionEventHandler>();
+	
+	private LoadAddSectionAction action;
+	
+	public LoadAddSectionEvent(LoadAddSectionAction action)
+	{
+		this.action = action;
+	}
+	
+	public LoadAddSectionAction getAction()
+	{
+		return action;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public Type<LoadAddSectionEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	/*
+	 * 
+	 */
+	@Override
+	protected void dispatch(LoadAddSectionEventHandler handler)
+	{
+		handler.onLoadAddSection(this);
+	}
 
 }
