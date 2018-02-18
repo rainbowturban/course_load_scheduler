870ab28b8cc574bddbadeddf83e0392e7ed30266
kms80 <xuebai80@yahoo.com>
2018-02-18 15:51:53 -0500

    Callback package and other stuff

---
+++ src/main/java/org/dselent/course_load_scheduler/client/callback/SendLoginCallback.java
@@ -0,0 +1,54 @@
+package org.dselent.course_load_scheduler.client.callback;
+
+import org.dselent.course_load_scheduler.client.action.InvalidLoginAction;
+import org.dselent.course_load_scheduler.client.action.ReceiveLoginAction;
+import org.dselent.course_load_scheduler.client.event.InvalidLoginEvent;
+import org.dselent.course_load_scheduler.client.event.ReceiveLoginEvent;
+import org.dselent.course_load_scheduler.client.translator.impl.LoginActionTranslatorImpl;
+import org.dselent.course_load_scheduler.client.utils.JSONHelper;
+
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public class SendLoginCallback extends DisplayCallback<JSONValue>
+{
+	public SendLoginCallback(SimpleEventBus eventBus, HasWidgets container)
+	{
+		super(eventBus, container);
+	}
+	  
+	@Override
+	public void onSuccess(JSONValue result)
+	{
+		JSONObject json = JSONHelper.getObjectValue(result);
+		LoginActionTranslatorImpl loginActionTranslator = new LoginActionTranslatorImpl();
+		ReceiveLoginAction action = loginActionTranslator.translateToAction(json);
+		
+		ReceiveLoginEvent event = new ReceiveLoginEvent(action, getContainer());
+		eventBus.fireEvent(event);
+	}
+	  
+	@Override
+	public void onFailure(Throwable caught)
+	{
+		// TODO
+		// give better exception information
+		// these stack traces are not helpful
+		
+		StringBuilder sb = new StringBuilder();
+		
+		StackTraceElement[] stackTraceElements = caught.getStackTrace();
+		for(StackTraceElement stackTraceElement : stackTraceElements)
+		{
+			sb.append(stackTraceElement.toString());
+			sb.append("\n");
+		}
+		
+		InvalidLoginAction ila = new InvalidLoginAction(sb.toString());
+		InvalidLoginEvent ile = new InvalidLoginEvent(ila);
+		eventBus.fireEvent(ile);
+	}
+
+  }
--- src/main/java/org/dselent/course_load_scheduler/client/event/InvalidLoginEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/InvalidLoginEvent.java
@@ -3,7 +3,6 @@ package org.dselent.course_load_scheduler.client.event;
 import org.dselent.course_load_scheduler.client.action.InvalidLoginAction;
 import org.dselent.course_load_scheduler.client.event_handler.InvalidLoginEventHandler;
 
-import com.google.gwt.event.shared.GwtEvent;
 
 /**
  * Events are "fired" and sent on the event bus to be sent to an applicable event handler
@@ -12,34 +11,23 @@ import com.google.gwt.event.shared.GwtEvent;
  * @author dselent
  *
  */
-public class InvalidLoginEvent extends GwtEvent<InvalidLoginEventHandler>
+public class InvalidLoginEvent extends BaseEvent<InvalidLoginAction, InvalidLoginEventHandler>
 {
 	public static Type<InvalidLoginEventHandler> TYPE = new Type<InvalidLoginEventHandler>();
-	
-	private InvalidLoginAction action;
-	
+		
 	public InvalidLoginEvent(InvalidLoginAction action)
 	{
-		this.action = action;
+		super(action);
 	}
 	
-	public InvalidLoginAction getAction()
-	{
-		return action;
-	}
 	
-	/*
-	 * 
-	 */
 	@Override
 	public Type<InvalidLoginEventHandler> getAssociatedType()
 	{
 		return TYPE;
 	}
 
-	/*
-	 * 
-	 */
+
 	@Override
 	protected void dispatch(InvalidLoginEventHandler handler)
 	{
