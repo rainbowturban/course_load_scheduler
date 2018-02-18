e7bc17a10c7a068990a4e733d35cea2e03d0a406
Sarah <saogrady@wpi.edu>
2018-02-17 19:19:51 -0500

    Home Stuff

--- src/main/resources/org/dselent/course_load_scheduler/client/view/impl/HomeViewImpl.java
+++ src/main/resources/org/dselent/course_load_scheduler/client/view/impl/HomeViewImpl.java
@@ -1,6 +1,8 @@
 package org.dselent.course_load_scheduler.client.view.impl;
 
+import org.dselent.course_load_scheduler.client.presenter.HomePresenter;
 import org.dselent.course_load_scheduler.client.presenter.ManageUserPresenter;
+import org.dselent.course_load_scheduler.client.view.HomeView;
 import org.dselent.course_load_scheduler.client.view.ManageUserView;
 
 import com.google.gwt.core.client.GWT;
@@ -11,6 +13,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 
 public class HomeViewImpl extends BaseViewImpl<HomePresenter> implements HomeView {
@@ -49,6 +52,21 @@ public class HomeViewImpl extends BaseViewImpl<HomePresenter> implements HomeVie
 
 	public HomeViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@Override
+	public void setPresenter(HomePresenter presenter) {
+		this.presenter = presenter;
+	}
+
+	@Override
+	public Widget getWidgetContainer() {
+		return this;
+	}
+
+	@Override
+	public HasWidgets getViewRootPanel() {
+		return rootPanel;
 	}
 
 }
