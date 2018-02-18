306e7f2f9b56d8da8e82d450ac6378160a2a7b9b
Sarah <saogrady@wpi.edu>
2018-02-17 21:53:26 -0500

    i made a dumb mistake

--- src/main/java/org/dselent/course_load_scheduler/client/action/ManageUserPageAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/ManageUserPageAction.java
@@ -1,5 +1,27 @@
 package org.dselent.course_load_scheduler.client.action;
 
 public class ManageUserPageAction {
+	boolean adminUser;//true if admin, false if not
+	
+	public ManageUserPageAction() {
+		adminUser = false;
+	}
+	
+	public ManageUserPageAction(boolean adminUser) {
+		this.adminUser = adminUser;
+	}
+	
+	public boolean getAdminUser() {
+		return adminUser;
+	}
 
+	public void setAdminUser(boolean adminUser) {
+		this.adminUser = adminUser;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Admin User: " + adminUser;
+	}
 }
