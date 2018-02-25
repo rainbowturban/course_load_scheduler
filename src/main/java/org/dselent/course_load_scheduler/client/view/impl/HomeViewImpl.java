7553377d4f207610b28384a95de0c5665da3944a
Frank Campanelli <rainbowturban412@gmail.com>
2018-02-24 19:14:26 -0500

    home view

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/HomeViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/HomeViewImpl.java
@@ -11,58 +11,64 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.event.logical.shared.AttachEvent;
 
 public class HomeViewImpl extends BaseViewImpl<HomePresenter> implements HomeView {
 
-	private static HomeViewImplUiBinder uiBinder = GWT.create(HomeViewImplUiBinder.class);
-
-	
-	@UiField Button homeButton;
-    @UiField Button scheduleButton;
-    @UiField Button coursesButton;
-    @UiField Button accountButton;
-    @UiField HTMLPanel rootPanel;
-    
-    @UiHandler("homeButton")
-    void onHomeButtonClick(ClickEvent event) {
-       presenter.loadHomePage();
-    }
-    
-    @UiHandler("scheduleButton")
-    void onScheduleButtonClick(ClickEvent event) {
-        presenter.loadSchedulePage();
-    }
-    
-    @UiHandler("coursesButton")
-    void onCoursesButtonClick(ClickEvent event) {
-        presenter.loadViewCoursesPage();
-    }
-    
-    @UiHandler("accountButton")
-    void onAccountButtonClick(ClickEvent event) {
-       presenter.loadAccountPage();
-    }
-    
 	interface HomeViewImplUiBinder extends UiBinder<Widget, HomeViewImpl> {
 	}
 
-	public HomeViewImpl() {
+	
+	private static HomeViewImplUiBinder uiBinder = GWT.create(HomeViewImplUiBinder.class);
+    @UiField Button accountButton;
+    @UiField Button coursesButton;
+    @UiField Grid grid;
+    @UiField Button homeButton;
+    @UiField HTMLPanel rootPanel;
+    
+    @UiField Button scheduleButton;
+    
+    public HomeViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
+    
+    @Override
+	public HasWidgets getViewRootPanel() {
+		return rootPanel;
+	}
+    
+    @Override
+	public Widget getWidgetContainer() {
+		return this;
+	}
+    
+	@UiHandler("accountButton")
+    void onAccountButtonClick(ClickEvent event) {
+       presenter.loadAccountPage();
+    }
+
+	@UiHandler("coursesButton")
+    void onCoursesButtonClick(ClickEvent event) {
+        presenter.loadViewCoursesPage();
+    }
+
+	@UiHandler("grid")
+	void onGridAttachOrDetach(AttachEvent event) {
+	}
+
+	@UiHandler("homeButton")
+    void onHomeButtonClick(ClickEvent event) {
+       presenter.loadHomePage();
+    }
+
+	@UiHandler("scheduleButton")
+    void onScheduleButtonClick(ClickEvent event) {
+        presenter.loadSchedulePage();
+    }
 
 	@Override
 	public void setPresenter(HomePresenter presenter) {
 		this.presenter = presenter;
 	}
-
-	@Override
-	public Widget getWidgetContainer() {
-		return this;
-	}
-
-	@Override
-	public HasWidgets getViewRootPanel() {
-		return rootPanel;
-	}
-
 }
