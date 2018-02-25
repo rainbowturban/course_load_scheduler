365447b44c91b40f3bc87a759ec4ed5820cfeb4d
Matt <mjschueler@gmail.com>
2018-02-25 18:37:44 -0500

    damn daniel, back at it again with the working request

--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/GetStartTimesActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/GetStartTimesActionTranslatorImpl.java
@@ -1,20 +1,13 @@
 package org.dselent.course_load_scheduler.client.translator.impl;
 
 import org.dselent.course_load_scheduler.client.action.ReceiveStartTimesAction;
-import org.dselent.course_load_scheduler.client.action.SendCreateAccountAction;
 import org.dselent.course_load_scheduler.client.model.StartTime;
-import org.dselent.course_load_scheduler.client.model.User;
-import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveCreateAccountKeys;
-import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveStartTimesKeys;
 import org.dselent.course_load_scheduler.client.send.jsonkeys.GetStartTimesKeys;
-import org.dselent.course_load_scheduler.client.send.jsonkeys.SendCreateAccountKeys;
 
 import java.sql.Time;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.dselent.course_load_scheduler.client.action.SendGetStartTimesAction;
-import org.dselent.course_load_scheduler.client.action.ReceiveCreateAccountAction;
 import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
 import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
@@ -44,34 +37,22 @@ public class GetStartTimesActionTranslatorImpl implements ActionTranslator<SendG
 		// you may choose to handle the exception as you wish
 
 		// sent timestamps as epoch seconds (long)
-
 		JSONValue jsonObject = json.get("success");
-		JSONObject userObject = jsonObject.isArray().get(0).isObject();
-
-		//Server only sends email and password back
-		Integer id = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(ReceiveStartTimesKeys.ID));
-		String password = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(ReceiveStartTimesKeys.PASSWORD));
-		Integer accTypeId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(ReceiveStartTimesKeys.ACCOUNT_TYPE_ID));
-		JSONArray jsonTimes = JSONHelper.getArrayValue(userObject, JSONHelper.convertKeyName(ReceiveStartTimesKeys.TIMES));
-		
-		User user = new User();
-		user.setId(id);
-		user.setPassword(password);
-		user.setAccountTypeId(accTypeId);
+		JSONArray userObject = jsonObject.isArray().get(0).isArray();
 		
 		ArrayList<StartTime> times = new ArrayList<StartTime>();
-		for (int i=0; i<jsonTimes.size(); i++) {
-			JSONValue tempTime = jsonTimes.get(i);
+		for (int i=0; i<userObject.size(); i++) {
+			JSONValue tempTime = userObject.get(i);
 			Integer timeId = JSONHelper.getIntValue(tempTime.isObject(), "id");
-			long timeVal = JSONHelper.getLongValue(tempTime.isObject(), "val");
-			Time t = new Time(timeVal);
+			String timeVal = JSONHelper.getStringValue(tempTime.isObject(), "time");
+			Time t = Time.valueOf(timeVal);
 			StartTime st = new StartTime();
 			st.setId(timeId);
 			st.setTime(t);
 			times.add(st);
 		}
 
-		ReceiveStartTimesAction action = new ReceiveStartTimesAction(user, times);	
+		ReceiveStartTimesAction action = new ReceiveStartTimesAction(times);
 
 		return action;
 	}
