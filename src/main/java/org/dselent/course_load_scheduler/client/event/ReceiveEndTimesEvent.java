b513b1b886c6b47b19a8a96867748b7b7cff750a
Matt <mjschueler@gmail.com>
2018-02-26 04:23:49 -0500

    all the mapping requests for the middle panel

--- src/main/java/org/dselent/course_load_scheduler/client/event/ReceiveEndTimesEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/ReceiveEndTimesEvent.java
@@ -3,13 +3,15 @@ package org.dselent.course_load_scheduler.client.event;
 import org.dselent.course_load_scheduler.client.action.ReceiveEndTimesAction;
 import org.dselent.course_load_scheduler.client.event_handler.ReceiveEndTimesEventHandler;
 
-public class ReceiveEndTimesEvent extends BaseEvent<ReceiveEndTimesAction, ReceiveEndTimesEventHandler> {
+import com.google.gwt.event.shared.GwtEvent;
+
+public class ReceiveEndTimesEvent extends GwtEvent<ReceiveEndTimesEventHandler> {
 	public static Type<ReceiveEndTimesEventHandler> TYPE = new Type<ReceiveEndTimesEventHandler>();
 	private ReceiveEndTimesAction action;
 
 	public ReceiveEndTimesEvent(ReceiveEndTimesAction action)
 	{
-		super(action);
+		this.action = action;
 	}
 
 	public ReceiveEndTimesAction getAction()
